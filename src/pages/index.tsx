72de9921029835d687a7af475f05fd7967693307
dikiogres <dikiogres@gmail.com>
2023-01-12 00:49:16 +0700

    feat: home mockup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Layout from '@/components/layout/Layout'
 
-const inter = Inter({ subsets: ['latin'] })
+import PhoneMockup from '../../public/phone-mockup.png'
 
 export default function Home() {
   return (
@@ -15,10 +15,26 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-      <main className=''>
-        <section>
-          <div>
-            <h1>Welcome, Dog!</h1>
+      <main className='overflox-x-hidden'>
+        <section className='min-h-screen '>
+          <div className='flex sm:flex-row items-center justify-center'>
+            <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+                <div className="mr-auto place-self-center lg:col-span-7">
+                    <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-[#FF0000]">RPL</h1>
+                    <h1 className="max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none md:text-5xl xl:text-6xl text-black">for everyone</h1>
+                    <p className="max-w-2xl mb-6 font-sm text-gray-700 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Find your future at RPL.</p>
+                    <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-black rounded-lg bg-white border-2 border-black hover:bg-black hover:text-white">
+                        Pelatihan
+                        <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    </a>
+                    <a href="#" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-400 bg-gray-300 rounded focus:outline-none cursor-not-allowed">
+                        Oprec Camin
+                    </a> 
+                </div>
+                <div className="lg:mt-0 lg:col-span-5 sm:flex order-first sm:order-last ">
+                    <Image src={PhoneMockup} alt="mockup" width={512} height={512}/>
+                </div>                
+            </div>
           </div>
         </section>
       </main>

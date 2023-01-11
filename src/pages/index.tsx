c4ff6691eba4c76210c0f5c8babf465b48010927
dikiogres <dikiogres@gmail.com>
2023-01-11 23:52:05 +0700

    feat: header & footer init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
+import Layout from '@/components/layout/Layout'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,6 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Layout>
       <main className=''>
         <section>
           <div>
@@ -20,6 +22,7 @@ export default function Home() {
           </div>
         </section>
       </main>
+      </Layout>
     </>
   )
 }

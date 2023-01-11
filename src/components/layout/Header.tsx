4eaeeb2d8f4655aad183029ba9c8fcdcbb78de8a
dikiogres <dikiogres@gmail.com>
2023-01-12 00:04:51 +0700

    feat: header & footer content

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='sticky top-0 z-50 border-b border-gray-300'>
+    <div className='container mx-auto px-4 sm:px-6 py-4'>
+      <Link href='/' className='font-bold md:text-2xl hover:text-gray-600'>
+        LABSE
+      </Link>
+    </div>
+  </header>
   )
 }
 

30ca8183a8561f6405e0bc5be34bfd5622106854
dikiogres <dikiogres@gmail.com>
2023-01-12 01:17:56 +0700

    feat: darkmode

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,17 +1,50 @@
+import React, {useState, useEffect} from 'react'
 import Link from 'next/link'
-import React from 'react'
+
+import {useTheme} from 'next-themes'
+import {MoonIcon, SunIcon} from '@heroicons/react/solid'
 
 
 const Header = () => {
-  return (
-    <header className='sticky top-0 z-50 border-b border-gray-300'>
-    <div className='container mx-auto px-4 sm:px-6 py-4'>
-      <Link href='/' className='font-bold md:text-2xl hover:text-gray-600'>
-        LABSE
-      </Link>
-    </div>
-  </header>
-  )
+
+    const {systemTheme, theme, setTheme } = useTheme();
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(()=>{
+        setMounted(true);
+    },[]);
+
+    const renderThemeChanger = () => {
+        if(!mounted) return null;
+        
+        const currentTheme = theme == 'system' ? systemTheme : theme;
+
+        if(currentTheme == 'dark'){
+            return(
+                <SunIcon className="w-7 h-7" role="button" onClick={() => setTheme
+                    ('light')
+                }/>
+            )
+        }else{
+            return(
+                <MoonIcon className="w-7 h-7" role="button" onClick={() => setTheme
+                    ('dark')
+                }/>
+            )
+        }
+    }
+
+    return (
+        <header className='sticky top-0 z-50 border-b  border-gray-300 dark:border-gray-700'>
+        <div className='container mx-auto px-4 sm:px-6 py-4 flex justify-between items-center'>
+            <Link href='/' className='font-bold md:text-2xl hover:text-gray-600'>
+                LABSE
+            </Link>
+
+            {renderThemeChanger()}
+        </div>
+    </header>
+    )
 }
 
 export default Header
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,9 +21,9 @@ export default function Home() {
             <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
                 <div className="mr-auto place-self-center lg:col-span-7">
                     <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-[#FF0000]">RPL</h1>
-                    <h1 className="max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none md:text-5xl xl:text-6xl text-black">for everyone</h1>
+                    <h1 className="max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none md:text-5xl xl:text-6xl text-black dark:text-white">for everyone</h1>
                     <p className="max-w-2xl mb-6 font-sm text-gray-700 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Find your future at RPL.</p>
-                    <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-black rounded-lg bg-white border-2 border-black hover:bg-black hover:text-white">
+                    <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-black rounded-lg bg-white border-2 border-black hover:bg-black hover:text-white dark:text-white dark:bg-gray-900 dark:border-white dark:hover:bg-white dark:hover:text-black ">
                         Pelatihan
                         <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                     </a>

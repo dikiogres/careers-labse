fbc78ad0f94011b5088e48cccfdda153eccd15d2
dikiogres <dikiogres@gmail.com>
2023-01-16 02:17:17 +0700

    feat: logo

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,6 +3,9 @@ import Link from 'next/link'
 
 import {useTheme} from 'next-themes'
 import {MoonIcon, SunIcon} from '@heroicons/react/solid'
+import Image from 'next/image'
+
+import Logo from '~/Color.png'
 
 
 const Header = () => {
@@ -38,7 +41,15 @@ const Header = () => {
         <header className='sticky top-0 z-50 border-b  border-gray-300 dark:border-gray-700'>
         <div className='container mx-auto px-4 sm:px-6 py-4 flex justify-between items-center'>
             <Link href='/' className='font-bold md:text-2xl hover:text-gray-600'>
-                LABSE
+                <div className='flex justify-center items-center'>
+                <Image
+                src={Logo}
+                alt='logo'
+                width={50}
+            />
+                <p className='pl-2'>LABSE</p>
+
+                </div>
             </Link>
 
             {renderThemeChanger()}

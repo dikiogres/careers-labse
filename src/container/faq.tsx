9b4e5060fb0668199d59e775087d467f666d1acf
Muhammad Yunus <Yunusenvire@gmail.com>
2023-02-02 16:41:50 +0700

    fix: unresponsive section

--- src/container/faq.tsx
+++ src/container/faq.tsx
@@ -8,12 +8,12 @@ import { qa }from "../data/faq.data"
 export default function Faq() {
   return (
     <div className="w-full pb-16 gap-y-8 grid md:grid-cols-2 items-center overflow-hidden flex w-11/12 mx-auto">
-      <div className="py-4 text-stroke dark:text-stroke-dark uppercase dark:text-white text-[32vw] md:text-[18.65rem] font-mono font-extrabold opacity-30 -z-10 leading-[18vw] md:leading-[9.6rem]">
+      <div className="py-4 px-1 text-stroke dark:text-stroke-dark uppercase dark:text-white text-[25vw] md:text-[18.65rem] font-mono font-extrabold opacity-30 -z-10 leading-[15vw] md:leading-[9.6rem]">
         <p>RPL</p>
         <p className="text-end md:translate-x-24">oprec</p>
         <p className="md:translate-x-8">admin</p>
       </div>
-      <div className="w-full rounded-2xl bg-white/70 dark:bg-slate-800  shadow-sm p-4 h-fit transition-all space-y-2 relative pt-8 mt-8 ">
+      <div className="w-full rounded-2xl bg-white/70 dark:bg-slate-800/70  shadow-sm p-4 h-fit transition-all space-y-2 relative pt-8 mt-8 ">
         <h2 className="absolute -top-8 text-6xl font-bold text-red-400 dark:text-teal-500">FAQ</h2>
         {qa.map((item, i) => {
           return (
@@ -21,11 +21,11 @@ export default function Faq() {
               {({ open }) => (
                 <>
                   <Disclosure.Button className="flex w-full items-center justify-between rounded-lg dark:text-white bg-gray-100 dark:bg-slate-600 px-4 py-2 text-left font-medium text-gray-900 hover:bg-gray-200 focus:outline-none focus-visible:ring focus-visible:ring-gray-500 focus-visible:ring-opacity-75">
-                    <h5 className="text-lg">{item.question}</h5>
+                    <h5 className="text-md sm:text-lg">{item.question}</h5>
                     <ChevronDownIcon
                       className={`${
                         open ? "rotate-180 transform" : ""
-                      } h-5 w-5 text-gray-500 transition-all`}
+                      } h-6 w-6 text-gray-500 transition-all flex-shrink-0`}
                     />
                   </Disclosure.Button>
                   <Transition

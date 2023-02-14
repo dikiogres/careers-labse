53d1277138869f4abf80cc6770afcf42fd168469
dikiogres <dikiogres@gmail.com>
2023-02-14 15:11:25 +0700

    feat: linktree

---
+++ src/data/cardLink.data.tsx
@@ -0,0 +1,40 @@
+import { SiGooglesheets, SiYoutube, SiGithub } from 'react-icons/si'
+
+export const CardLinkData = [
+    { 
+      id: 1,
+      link: '',
+      title: 'Timeline',
+      icon: <SiGooglesheets/>
+    },
+    { 
+      id: 2,
+      link: 'https://www.youtube.com/playlist?list=PLbVhZXO7113G-omiOz7c3i7i7NP37MikS',
+      title: 'Record Materi FE',
+      icon: <SiYoutube/>
+    },
+    { 
+      id: 3,
+      link: 'https://www.youtube.com/playlist?list=PLbVhZXO7113H-6i-si0hJTeILxVyvT4Bf',
+      title: 'Record Materi BE',
+      icon: <SiYoutube/>
+    },
+    { 
+      id: 4,
+      link: 'https://docs.google.com/spreadsheets/d/17h_xyLIwOkTmBWlUX1wiz9U1-hn3FP2qxJSoMP-83NU/edit#gid=504585426',
+      title: 'Informasi Plotting',
+      icon: <SiGooglesheets/>
+    },
+    { 
+      id: 5,
+      link: 'https://github.com/dzikrisyairozi/modul-next-ts/tree/main/modul-1',
+      title: 'Modul 1 FE',
+      icon: <SiGithub/>
+    },
+    { 
+      id: 6,
+      link: 'https://github.com/godlixe/modul-go/blob/main/dasar-dasar-golang.md',
+      title: 'Modul 1 BE',
+      icon: <SiGithub/>
+    },
+  ]
